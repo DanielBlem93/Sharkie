fb3849bdc484a36816c2789dcea8c909020726fb
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2023-09-02 16:25:23 +0200

    refactored

--- src/js/models/character-class.js
+++ src/js/models/character-class.js
@@ -54,61 +54,36 @@ class Character extends MovableObjekt {
 
     constructor() {
         super()
-        this.loadImage('src/img/2_character_pepe/5_dead/D-57.png')
+        this.loadImage('src/img/2_character_pepe/2_walk/W-21.png')
         this.loadImages(this.IMAGES_WALKING)
         this.loadImages(this.IMAGES_JUMPING)
         this.loadImages(this.IMAGES_DEAD)
         this.loadImages(this.IMAGES_HURT)
         this.applyGravity(this.isCharacterAboveGround)
         this.animate()
+
     }
+
+
+
     animate() {
         setInterval(() => {
             this.walking_sound.pause()
-            if (this.world.keyboard.right && this.x < this.world.level.level_end_x && !this.dead) {
-                this.otherDirection = false
-                this.moveRight()
-                this.walking_sound.play()
-            }
-
-            else if (this.world.keyboard.left && this.x > 0 && !this.dead) {
-                this.otherDirection = true
-                this.moveLeft()
-                this.walking_sound.play()
-            }
-
-            if (this.world.keyboard.space && !this.isCharacterAboveGround() && !this.dead) {
-                this.jump()
-            }
-
-            this.world.camera_x = -this.x + 100
+            this.walkingRight()
+            this.walkingLeft()
+            this.jumping()
+            this.fixCameraOnCharacter()
         }, 1000 / 30);
 
         setInterval(() => {
 
-            if (this.godmode && !this.dead) {
-                this.playAnimation(this.IMAGES_HURT)
+            this.hurtAnimation()
+            this.playWalkAnimation()
 
-            }
-
-            else if (this.died) {
-
-                this.loadImage('src/img/2_character_pepe/5_dead/D-57.png')
-            }
-
-            else {
-
-                if (this.world.keyboard.right && !this.dead || this.world.keyboard.left && !this.dead) {
-
-                    //walk animation
-                    this.playAnimation(this.IMAGES_WALKING)
-                }
-            }
         }, 60);
 
         this.dieing()
-        this.jumping()
-
+        this.jumpingAnimation()
 
     }
 
@@ -121,25 +96,76 @@ class Character extends MovableObjekt {
 
                 } else {
                     this.died = true
+                    this.disableCharacter()
                 }
-
             }
         }, 1000 / 7);
     }
+   
+
+    hurtAnimation() {
+        if (this.godmode && !this.dead) {
+            this.playAnimation(this.IMAGES_HURT)
+        }
+    }
+
+    playWalkAnimation() {
+
+        if (this.world.keyboard.right && !this.dead && !this.isCharacterAboveGround() ||
+            this.world.keyboard.left && !this.dead && !this.isCharacterAboveGround()) {
+
+            this.playAnimation(this.IMAGES_WALKING)
+        }
+    }
+
+    disableCharacter() {
+        if (this.died) {
+            this.loadImage('src/img/2_character_pepe/5_dead/D-57.png')
+        }
+    }
+
+    fixCameraOnCharacter() {
+        this.world.camera_x = -this.x + 100
+    }
+
+    playWalkingSound() {
+        if (!this.isCharacterAboveGround())
+            this.walking_sound.play()
+    }
+
+    walkingRight() {
+        if (this.world.keyboard.right && this.x < this.world.level.level_end_x && !this.dead) {
+            this.otherDirection = false
+            this.moveRight()
+            this.playWalkingSound()
+        }
+    }
+
+    walkingLeft() {
+        if (this.world.keyboard.left && this.x > 0 && !this.dead) {
+            this.otherDirection = true
+            this.moveLeft()
+            this.playWalkingSound()
+        }
+    }
+
     jumping() {
+        if (this.world.keyboard.space && !this.isCharacterAboveGround() && !this.dead) {
+            this.jump()
+        }
+    }
+
+    jumpingAnimation() {
 
         setInterval(() => {
             if (this.isCharacterAboveGround() && !this.dead) {
                 if (this.s < this.IMAGES_JUMPING.length)
                     this.playJumpAnimation(this.IMAGES_JUMPING)
-            } 
+            }
             else if (!this.isCharacterAboveGround()) {
                 this.s = 0
             }
         }, 1000 / 9);
-
-
-
     }
 
 
